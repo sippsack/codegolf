3fbfc263165a1fe79ac62f8860eb72c0ea6d42d2
Niko Köbler <niko@n-k.de>
2016-07-15 08:47:04 +0200

    add first ui fragments for testing

--- src/main/java/dasniko/CodegolfApplication.java
+++ src/main/java/dasniko/CodegolfApplication.java
@@ -1,9 +1,11 @@
 package dasniko;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CodegolfApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dasniko/StaticResourceController.java
@@ -0,0 +1,17 @@
+package dasniko;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Niko Köbler, http://www.n-k.de, @dasniko
+ */
+@Controller
+@RequestMapping("/")
+public class StaticResourceController {
+
+    @RequestMapping
+    public String index() {
+        return "index";
+    }
+}
