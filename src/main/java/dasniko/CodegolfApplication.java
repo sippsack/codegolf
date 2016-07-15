190307342e089bc7f1f03b856c8e35700ba32b3a
Niko Köbler <niko@n-k.de>
2016-07-15 07:50:59 +0200

    init project

---
+++ src/main/java/dasniko/CodegolfApplication.java
@@ -0,0 +1,12 @@
+package dasniko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegolfApplication.class, args);
+	}
+}
