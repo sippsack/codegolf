4c426dbcdd6ba536e9b67102aa9d29f27e7cb16c
Niko Köbler <niko@n-k.de>
2016-12-21 11:14:50 +0100

    update project

--- src/main/java/de/dasniko/codegolf/CodegolfApplication.java
+++ src/main/java/de/dasniko/codegolf/CodegolfApplication.java
@@ -1,11 +1,9 @@
 package de.dasniko.codegolf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class CodegolfApplication {
 
 	public static void main(String[] args) {
