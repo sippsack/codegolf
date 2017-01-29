9f873c9a0d52355e4a2c625aa3b1bb2176b26700
Niko Köbler <niko@n-k.de>
2017-01-29 13:17:18 +0100

    change language

--- src/main/java/de/dasniko/codegolf/CodegolfApplication.java
+++ src/main/java/de/dasniko/codegolf/CodegolfApplication.java
@@ -5,7 +5,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.representations.AccessToken;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
@@ -18,6 +18,9 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.servlet.Filter;
@@ -29,7 +32,7 @@ import java.util.Locale;
  */
 @SpringBootApplication
 @EnableDynamoDBRepositories
-public class CodegolfApplication {
+public class CodegolfApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(CodegolfApplication.class, args);
@@ -43,13 +46,25 @@ public class CodegolfApplication {
     }
 
     @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
     public Filter registerRequestLoggingFilter() {
         return new RequestLoggingFilter();
     }
 
     @Bean
     public AmazonS3 amazonS3() {
-        return new AmazonS3Client();
+        return AmazonS3ClientBuilder.defaultClient();
     }
 
     @Bean
